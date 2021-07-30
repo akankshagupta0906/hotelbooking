f23573869025f70ce8aed5d9976a0350e404da6a
akanksha503gupta <83533236+akanksha504gupta@users.noreply.github.com>
2021-07-30 13:35:17 -0400

    hotel

--- HotelBooking/settings.py
+++ HotelBooking/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 MEDIA_URL='/media/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND ='django.core.mail.backends.smpt.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
